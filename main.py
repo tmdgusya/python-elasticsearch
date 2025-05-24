6c404f6b88f1870fe4ef2d0fca1437a165ad6ea0
tmdgusya <dev0jsh@gmail.com>
2025-05-24 19:15:42 +0900

    add es dep

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-def main():
-    print("Hello from python-elasticsearch!")
+from dataclasses import dataclass
+from elasticsearch import Elasticsearch
 
+@dataclass
+class UserDocument:
+    id: int
+    name: str
+    email: str
+
+def main():
+    client = Elasticsearch(
+        "http://localhost:9200",
+        api_key="api_key",
+    )
+
+    if not client:
+        raise Exception("Elasticsearch client not initialized")
+
+    try:
+        info = client.info()
+        print(info)
+    finally:
+        client.close()
 
 if __name__ == "__main__":
     main()
